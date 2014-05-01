8dbb02eff541837f76093f5e2fa1c7d8adc929a0
paulo-surfdome <paulo.marques@surfdome.com>
2014-05-01 15:35:45 +0100

    Update rubocop_listener.py

--- rubocop_listener.py
+++ rubocop_listener.py
@@ -72,7 +72,7 @@ class RubocopEventListener(sublime_plugin.EventListener):
         view_dict[ln] = message
         line = view.line(view.text_point(ln, 0))
         lines.append(sublime.Region(line.begin(), line.end()))
-    view.add_regions(REGIONS_ID, lines, '', 'circle')
+    view.add_regions(REGIONS_ID, lines, 'invalid.deprecated', 'circle')
 
   def run_rubocop(self, path):
     s = sublime.load_settings(SETTINGS_FILE)
